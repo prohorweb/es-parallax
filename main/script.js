671c66bfa121475361aef84557bc6336e35bbcf0
prohorweb <prohorweb@gmail.com>
2025-03-26 21:02:27 +0300

    Front club

--- main/script.js
+++ main/script.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', function() {
     const video = document.querySelector('header.main video');
     let scrollValue = window.scrollY;
 
-    heroContent.style.transform = `scale(${1 + scrollValue * .075})`;
+    heroContent.style.transform = `scale(${1 + scrollValue * .05})`;
     video.style.transform = `scale(${1 + scrollValue * .0005})`;
     
 });
